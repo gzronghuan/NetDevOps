02e87ced884328ff0b92bbe347acdd269f33086a
桓 荣 <huanrong@huandeMacBook-Pro.local>
2019-04-15 21:22:58 +0800

    commit3

--- 第六周实验/6-4-xml.py
+++ 第六周实验/6-4-xml.py
@@ -15,11 +15,14 @@ phone ={}
 for p_dict in phone_dict:
     phone[i] = p_dict
 #    print ('手机' + str(i),phone[i])
+    name = doc.createElement('产品')
+    name.setAttribute('name','手机' + str(i))
+    root.appendChild(name)
     for key in phone[i]:
 #        print (phone[i][key])
-        name = doc.createElement(key)
-        name.setAttribute('name',phone[i][key])
-        root.appendChild(name)
+        phone_name = doc.createElement(key)
+        phone_name.setAttribute('name',phone[i][key])
+        name.appendChild(phone_name)
     i += 1
 
 XML_File = open('XML_Phone.xml', 'w',  encoding='utf-8')
